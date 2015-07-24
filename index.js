821fe9c9fc013fa97c82ffbf9e828232cfee59f7
thegreat1200 <junk_mail000@icloud.com>
2015-07-24 07:42:49 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+(function () {
+API.on(API.CHAT, checkCommand);
+API.on(API.SCORE_UPDATE, checkScore);
+
+function checkCommand(data) {
+if (data.type === "message" && data.message === "!kill") {
+API.off(API.CHAT, checkCommand);
+API.sendChat("Deactivated.");
+}
+}
+
+var grabbed = "";
+
+function checkScore(data) {
+if (data.positive >= (API.getAudience().length/2) && data.id !== grabbed) {
+$("div#grab").click();
+$($(".grab .menu ul li")[0]).mousedown();
+API.sendChat(API.getMedia().title+" By: "+API.getMedia().author+" was grabbed! Thank You, @"+API.getDJ().username+"!");
+grabbed = data.id;
+}
+}
+}).call(this);
