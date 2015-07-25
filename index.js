428249a49592f8495a2f71672761a699655ad08d
thegreat1200 <junk_mail000@icloud.com>
2015-07-25 09:15:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ function checkScore(data) {
 if (data.positive >= (API.getAudience().length/2) && data.id !== grabbed) {
 $("div#grab").click();
 $($(".grab .menu ul li")[0]).mousedown();
-//API.sendChat(API.getMedia().title+" By: "+API.getMedia().author+" was grabbed! Thank You, @"+API.getDJ().username+"!");
+API.sendChat(API.getMedia().title+" By: "+API.getMedia().author+" was grabbed! Thank You, @"+API.getDJ().username+"!");
 grabbed = data.id;
 }
 }
