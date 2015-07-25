59ae2b5d206d3dc0de82522742b9b5782fbd375d
thegreat1200 <junk_mail000@icloud.com>
2015-07-24 20:05:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,51 @@
 API.on(API.CHAT, checkCommand);
 API.on(API.SCORE_UPDATE, checkScore);
 
+var botRank = API.getUser().role;
+
+var roomRank = 0;
+var globalRole = 0;
+var userRole = 0;
+// userRole: 1 User, 2 RDJ, 3 Bouncer, 4 Manager, 5 CoOwner, 6 Owner, 7 BA, 8 Admin
+
 function checkCommand(data) {
 if (data.type === "message" && data.message.charAt(0) === "!") {
+  roomRank = API.getUser().role;
+  globalRole = API.getUser().gRole;
+  //if (botRank > 1) {
+  if (data.id === "5626305") {
+    userRole = 4;
+  } else if (globalRole > 0) {
+    if (globalRole < 4 && globalRole > 2) {
+      userRole = 7;
+    } else if (globalRole === 5) {
+      userRole = 8;
+    }
+  } else {
+    if (roomRank === 0) {
+      userRole = 1;
+    } else if (roomRank === 1) {
+      userRole = 2;
+    } else if (roomRank === 2) {
+      userRole = 3;
+    } else if (roomRank === 3) {
+      userRole = 4;
+    } else if (roomRank === 4 || roomRank === 5) {
+      userRole = 5;
+    }
+  }
+  /*} else {
+    API.chatLog("Can't run without Bouncer or higher.");
+  }*/
   switch (data.message) {
     case "!kill":
+      if (userRole > 4) {
       API.off(API.CHAT, checkCommand);
       API.off(API.SCORE_UPDATE, checkScore);
       API.sendChat(API.getUser().username+" Deactivated.")
+      } else {
+        API.sendChat("@"+data.un+" you don't have permission!");
+      }
       break;
     case "!cookie":
       API.sendChat(data.un+" eats a Cookie!");
